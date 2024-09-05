8f2ecb6a04d514c1f145839e17fae5ffce4da837
Aditya <lapratomo24@yahoo.com>
2024-09-05 22:45:29 +0200

    Used variables for list

--- squirrels.py
+++ squirrels.py
@@ -3,16 +3,16 @@ import pandas
 data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
 print(data)
 
-grey_count = data[data["Primary Fur Color"] == "Gray"]
-print(len(grey_count))
-cinnamon_count = data[data["Primary Fur Color"] == "Cinnamon"]
-print(len(cinnamon_count))
-black_count = data[data["Primary Fur Color"] == "Black"]
-print(len(black_count))
+grey_count = len(data[data["Primary Fur Color"] == "Gray"])
+print(grey_count)
+cinnamon_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+print(cinnamon_count)
+black_count = len(data[data["Primary Fur Color"] == "Black"])
+print(black_count)
 
 data_dict = {
-    "squirrels": ["Gray", "Cinnamon", "Black"],
-    "count": [2473, 392, 103]
+    "Squirrels": ["Gray", "Cinnamon", "Black"],
+    "Count": [grey_count, cinnamon_count, black_count]
 }
 
 conversion = pandas.DataFrame(data_dict)
